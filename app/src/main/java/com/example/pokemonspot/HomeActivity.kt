4a6d6d68639b1c738ea1882f4f09e908ee70c6df
Richard <richardanemam@gmail.com>
2022-02-02 17:31:28 -0300

    set up initial themes

--- app/src/main/java/com/example/pokemonspot/HomeActivity.kt
+++ app/src/main/java/com/example/pokemonspot/HomeActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
