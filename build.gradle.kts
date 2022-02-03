0a874d8dc8bf18fe16b9cfb0eddd5e328440eb86
Richard <richardanemam@gmail.com>
2022-02-02 22:06:49 -0300

    create feature module and profile item

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
     dependencies {
         classpath(Libs.gradleTools)
         classpath(Libs.kotlinPlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
