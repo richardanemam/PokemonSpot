bc888c4cb877801c6ceaea3d99bb07b07e49572a
Richard <richardanemam@gmail.com>
2022-02-06 17:46:43 -0300

    create layout for details

---
+++ features/pokemon/src/main/java/com/pokemon/presentation/activity/pokemondetails/PokemonDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.pokemon.presentation.activity.pokemondetails
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PokemonDetailsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
